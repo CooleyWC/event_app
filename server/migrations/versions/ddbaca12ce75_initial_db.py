f409030446663e1709cec839579653dca1ae8096
Will Cooley <william.cooley@gmail.com>
2024-10-31 22:30:29 -0400

    initial commit

---
+++ server/migrations/versions/ddbaca12ce75_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: ddbaca12ce75
+Revises: 
+Create Date: 2024-10-31 22:30:20.743672
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ddbaca12ce75'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
