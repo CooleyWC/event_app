56ac7bf9904eb415c873e47d4a9f358c399af569
Will Cooley <william.cooley@gmail.com>
2024-11-04 22:36:08 -0500

    added users table

--- server/app.py
+++ server/app.py
@@ -1,4 +1,10 @@
 from config import app
+from models.users import User
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return 'hey'
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
