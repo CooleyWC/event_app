a9f4f7d86294477f33c519381e6a4b4a3a97c5be
Will Cooley <william.cooley@gmail.com>
2024-11-11 21:59:29 -0500

    updated users model

--- server/models/users.py
+++ server/models/users.py
@@ -12,6 +12,7 @@ class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    location: so.Mapped[str] = so.mapped_column(sa.String(120), index=True)
 
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
