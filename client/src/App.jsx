274d17c97287e3138b12d6fd3caa6d8f1fa5ed87
Will Cooley <william.cooley@gmail.com>
2024-11-20 22:18:12 -0500

    created useAuth() hook - global user context and updated App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,31 @@ import ErrorPage from "./ErrorPage";
 import Layout from "./Layout";
 import Dashboard from "./components/pages/Dashboard";
 import Login from "./components/forms/Login";
+import {useAuth} from './components/context/AuthProvider'
 
 
 function App() {
+
+
+  const {login, user, update} = useAuth()
+
+  useEffect(()=>{
+    checkUser()
+  }, [])
+  
+  const checkUser = async () =>{
+    try{ 
+      const res = await fetch('/api/check_session')
+      const userData = await res.json()
+      if(res.ok){
+        login(userData)
+      } else {
+        logout()
+      }
+    } catch (error) {
+      console.error('Error - try logging in again', error.message)
+    }
+  }
 
   return (
     <Router>
