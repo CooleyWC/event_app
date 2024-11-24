74da2e124e79a3091cc76ca7c44bfb52a3f9d252
Will Cooley <william.cooley@gmail.com>
2024-11-24 14:34:02 -0500

    changed to dashboard to grid layout to handle responsiveness between side navigation and content area

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -10,54 +10,58 @@ function Dashboard({dropdownOpen}) {
 
     const {section} = useParams();
 
-    const sideWidth = 64
 
-    
     return (
-        <div>
-            {user && (
-                <div className='w-44 h-screen bg-indigo-800 text-zinc-200 hidden sm:fixed sm:block'>
-                    <SideDrawer />
-                </div>
-            )}
-            {user && dropdownOpen && (
+        <div className='sm:grid grid-cols-[200px_1fr] w-full h-screen'>
+            {dropdownOpen ? (
                 <div className='w-full h-screen fixed bg-sky-400 md:hidden'>
+                     <SideDrawer />
+                </div>
+            ) : (
+                <div className='h-screen bg-indigo-800 hidden sm:block text-zinc-200'>
                     <SideDrawer />
                 </div>
-            )}
-
+            )
+        }
+            <div className='p-4 overflow-auto'>
             {!section && (
-                <div>
-                    <h1 className='text-center'>No selection - content area empty</h1>
+                <div className='text-center'>
+                    <h1>No selection - content area empty</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae aut sit explicabo recusandae voluptatem sapiente illo, aliquam laboriosam! Eos corrupti sunt nobis eum neque error, iure assumenda labore quas.</p>
                 </div>
             )}
             {section === 'upcoming_events' && (
-                <div>
-                    <h1 className='text-center'>Upcoming Events</h1>
+                <div className='text-center'>
+                    <h1>Upcoming Events</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae aut sit explicabo recusandae voluptatem sapiente illo, aliquam laboriosam! Eos corrupti sunt nobis eum neque error, iure assumenda labore quas.</p>
                 </div>
             )}
             {section === 'past_events' && (
                 <div>
                     <h1 className='text-center'>Past Events</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae aut sit explicabo recusandae voluptatem sapiente illo, aliquam laboriosam! Eos corrupti sunt nobis eum neque error, iure assumenda labore quas.</p>
                 </div>
             )}
             {section === 'find_events' && (
                 <div>
                     <h1 className='text-center'>Find Events</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae aut sit explicabo recusandae voluptatem sapiente illo, aliquam laboriosam! Eos corrupti sunt nobis eum neque error, iure assumenda labore quas.</p>
                 </div>
             )}
             {section === 'create_event' && (
                 <div>
                     <h1 className='text-center'>Create Event</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae aut sit explicabo recusandae voluptatem sapiente illo, aliquam laboriosam! Eos corrupti sunt nobis eum neque error, iure assumenda labore quas.</p>
                 </div>
             )}
             {section === 'profile' && (
                 <div>
                     <h1 className='text-center'>Profile</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae aut sit explicabo recusandae voluptatem sapiente illo, aliquam laboriosam! Eos corrupti sunt nobis eum neque error, iure assumenda labore quas.</p>
                 </div>
             )}
 
-            
+        </div>
         </div>
     );
 }
