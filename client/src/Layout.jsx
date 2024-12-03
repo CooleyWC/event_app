82423bcce4bf0ebdf5f08f2e63e5a372f08f2dcc
Will Cooley <william.cooley@gmail.com>
2024-12-03 13:35:59 -0500

    added text to Layout component

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Layout() {
     return (
-        <div className='mt-40 h-screen'>
-            <h1>Layout</h1>
+        <div className='mt-20 h-screen flex flex-col gap-4'>
+            <h1 className='text-4xl text-center'>Parlor Stages</h1>
+            <h2 className='text-xl text-center'>Bring the Magic of Intimate Performances to Your Space</h2>
+            <p className='px-20 mx-4'>Experience the charm and elegance of early 20th-century parlor concerts with Parlor Stages. Whether you're hosting a cozy gathering or organizing a unique event, our platform makes it easy to connect with performers and create unforgettable experiences in the comfort of your home or a special venue. From solo recitals to small ensemble performances, rediscover the personal touch of live music. Host, attend, and enjoy concerts the way they were meant to be—up close and personal.</p>
         </div>
     );
 }
