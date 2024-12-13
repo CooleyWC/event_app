3194ec74e3f0ecb62252de25922147cb4c3cb43a
Will Cooley <william.cooley@gmail.com>
2024-12-13 13:46:13 -0500

    EventDetails now successfully gets a response from check_event_conflict

--- client/src/components/pages/EventDetail.jsx
+++ client/src/components/pages/EventDetail.jsx
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useParams, NavLink} from 'react-router-dom';
+import { useAuth} from '../context/AuthProvider'
+
 
 const EventDetail = () => {
+
+    const {user} = useAuth()
 
     const params = useParams()
  
@@ -24,8 +28,29 @@ const EventDetail = () => {
         return <p>...loading</p>
     }
 
-    const handleTicketClick = () =>{
-        console.log('purchase ticket for this event', eventID)
+    const handleTicketClick = async () =>{
+        try {
+            const response = await fetch('/api/check_event_conflict', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user_id: user.id,
+                    event_id: eventData.id,
+                    start_time: eventData.start_time,
+                    end_time: eventData.end_time,
+                    }) 
+            })
+            const result = await response.json()
+            if(result.conflict){
+                alert(result.message)
+                return
+            } 
+            console.log('approved', result.message)
+        } catch (error) {
+            console.error('there was a problem', error)
+        }
     }
 
     return (
