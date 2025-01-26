8966baef5a84363158efd3481d0514c1e4e016ef
Will Cooley <william.cooley@gmail.com>
2025-01-25 23:39:15 -0500

    create event in progress

--- client/src/components/forms/CreateEvent.jsx
+++ client/src/components/forms/CreateEvent.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 function CreateEvent() {
     return (
-        <div className='bg-blue-300 h-screen'>
-            <h1 className='text-black text-center'>Create Event</h1>
+        <div className='w-full pt-2 flex flex-col justify-center align-center'>
+            <h1 className='text-ivory text-center'>Create Event</h1>
+            <div className='w-full flex justify-center'>
+                <form>
+                    <div>
+                        <label>Event Name</label>
+                        <input />
+                    </div>
+                    <div>
+                        <label>Start Time</label>
+                        <input />
+                    </div>
+                    <div>
+                        <label>End Time</label>
+                        <input />
+                    </div>
+                    <div>
+                        <label>Capacity</label>
+                        <input />
+                    </div>
+                    <div>
+                        <label>Description</label>
+                        <input />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
