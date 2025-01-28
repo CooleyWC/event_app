1da7fcdf753e0bf12311d1c35b2e206267309f07
Will Cooley <william.cooley@gmail.com>
2025-01-27 23:27:35 -0500

    changed Main to min-h-screen which fixes the bottom cutting off on upcoming events and create event

--- client/src/ui/Main.jsx
+++ client/src/ui/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Main({children}) {
     return (
-        <div className='text-gray-500 bg-gray-100 dark:bg-slate-950 mt-16 pt-16 h-screen sm:ml-[16rem]'>
+        <div className='text-gray-500 bg-gray-100 dark:bg-slate-950 mt-16 pt-16 min-h-screen sm:ml-[16rem]'>
             {children}
         </div>
     );
