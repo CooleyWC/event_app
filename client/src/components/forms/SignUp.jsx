82f73adb2fbb723eb96686281fe5d5d5b16ac295
Will Cooley <william.cooley@gmail.com>
2025-03-02 11:20:39 -0500

    added signup form and route

--- client/src/components/forms/SignUp.jsx
+++ client/src/components/forms/SignUp.jsx
@@ -2,15 +2,84 @@ import React, {useState} from 'react';
 import {useAuth} from '../context/AuthProvider'
 import {useNavigate} from 'react-router-dom'
 
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
 function SignUp() {
 
     const {login} = useAuth()
 
     let navigate = useNavigate()
 
-    const handleSubmit = ()=>{
-        console.log('yo')
+    const submitUser = async (values) =>{
+
+        console.log('submittedValues', values)
+      
+        try {
+            const res = await fetch('/api/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-type': ' application/json'
+                },
+                body: JSON.stringify(values)
+            })
+
+            const userData = await res.json()
+            if(!res.ok){
+                console.log('error - signup failed', userData.error)
+                alert(userData.error)
+                return
+            }
+            console.log('signup success', userData)
+            login(userData)
+            navigate('/dashboard')
+        } catch (error){
+            console.error('error', error.message)
+            return error
+        }
     }
+
+    const signupSchema = yup.object({
+        firstName: yup
+            .string('Enter your first name')
+            .min(2, 'Must be at least 2 characters')
+            .max(15, 'Must be no bigger than 15 characters')
+            .required('First Name is required'),
+        lastName: yup
+            .string('Enter your last name')
+            .min(2, 'Must be at least 2 characters')
+            .max(15, 'Must be no bigger than 15 characters')
+            .required('Last Name is required'),
+        location: yup
+            .string('Enter your location (city and state)')
+            .min(3, 'Must be at least 3 characters')
+            .max(20, 'Must be no bigger than 15 characters')
+            .required('Location is required'),
+        email: yup
+            .string('Enter Your Email')
+            .email('Enter a valid Email')
+            .required('Email is Required'),
+        password: yup
+            .string('Enter your password')
+            .min(8, 'Must be at least 2 characters')
+            .max(20, 'Must be no bigger than 20 characters')
+            .required('Password is required'),
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            location: '',
+            password: '',
+            
+        },
+        validationSchema: signupSchema,
+        onSubmit: submitUser
+    })
+
+
 
     const handleLoginClick = ()=>{
 
@@ -27,16 +96,67 @@ function SignUp() {
             </div>
             
             <div className='mt-4 text-black dark:text-ivory sm:mx-auto sm:w-full sm:max-w-sm '>
-                <form onSubmit={handleSubmit} className='space-y-4'>
+                <form onSubmit={formik.handleSubmit} className='space-y-4'>
+                    <label htmlFor='firstName' className='block font-medium'>First Name</label>
+                    <input 
+                        type='text' 
+                        id='firstName' 
+                        name='firstName'
+                        placeholder='Kevin'
+                        className='pl-2 placeholder:italic border border-transparent focus:outline-none focus:ring-4 focus:ring-blue-600  focus:border-transparent block text-gray-900 w-full rounded-sm bg-white py-1.5 placeholder-gray-600' 
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                        
+                    />
+                    {formik.touched.firstName && formik.errors.firstName ? (
+                        <div className='text-red-600'>{formik.errors.firstName}</div>
+                    ) : null}
+
+                    <label htmlFor='firstName' className='block font-medium'>Last Name</label>
+                    <input 
+                        type='text' 
+                        id='lastName' 
+                        name='lastName'
+                        placeholder='McAllister'
+                        className='pl-2 placeholder:italic border border-transparent focus:outline-none focus:ring-4 focus:ring-blue-600  focus:border-transparent block text-gray-900 w-full rounded-sm bg-white py-1.5 placeholder-gray-600' 
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                        
+                    />
+                    {formik.touched.lastName && formik.errors.lastName ? (
+                        <div className='text-red-600'>{formik.errors.lastName}</div>
+                    ) : null}
+
+                    <label htmlFor='location' className='block font-medium'>Location</label>
+                    <input 
+                        type='text' 
+                        id='location' 
+                        name='location'
+                        placeholder='Chicago, IL'
+                        className='pl-2 placeholder:italic border border-transparent focus:outline-none focus:ring-4 focus:ring-blue-600  focus:border-transparent block text-gray-900 w-full rounded-sm bg-white py-1.5 placeholder-gray-600' 
+                        value={formik.values.location}
+                        onChange={formik.handleChange}
+                     
+                    />
+                    {formik.touched.location && formik.errors.location ? (
+                        <div className='text-red-600'>{formik.errors.location}</div>
+                    ) : null}
+            
                     <label htmlFor='email' className='block font-medium'>Email Address</label>
                     <input 
-                        type='email' 
+                        type='text' 
                         id='email' 
                         name='email'
                         placeholder='example@gmail.com'
                         className='pl-2 placeholder:italic border border-transparent focus:outline-none focus:ring-4 focus:ring-blue-600  focus:border-transparent block text-gray-900 w-full rounded-sm bg-white py-1.5 placeholder-gray-600' 
-                        onChange={(e)=>handleEmailChange(e)}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        
                     />
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className='text-red-600'>{formik.errors.email}</div>
+                    ) : null}
+
                     <label htmlFor='password' className='block font-medium'>Password</label>
                     <input 
                         type='password'
@@ -44,10 +164,15 @@ function SignUp() {
                         name='password'
                         placeholder='enter your password here'
                         className='pl-2 placeholder:italic border border-transparent focus:outline-none focus:ring-4 focus:ring-blue-600  focus:border-transparent block text-gray-900 w-full rounded-sm bg-white py-1.5 placeholder-gray-600' 
-                        onChange={(e)=>handlePasswordChange(e)}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                      
                     />
+                    {formik.touched.password && formik.errors.password ? (
+                        <div className='text-red-600'>{formik.errors.password}</div>
+                    ) : null}
                     <div>
-                        <button type='submit' className='flex mt-8 w-full justify-center py-6 rounded-md text-2xl font-semibold bg-gray-600 dark:text-white hover:bg-gray-700 hover:text-ivory transition ease-in-out duration-300 '>Login</button>
+                        <button type='submit' className='flex mt-8 w-full justify-center py-6 rounded-md text-2xl font-semibold bg-gray-600 dark:text-white hover:bg-gray-700 hover:text-ivory transition ease-in-out duration-300 '>Create Account</button>
                     </div>
                 </form>
                 <div className='mt-4 flex flex-col justify-center'>
--- server/app.py
+++ server/app.py
@@ -7,6 +7,7 @@ from flask_restful import Resource
 
 from resources.users import Users
 from resources.login import Login
+from resources.signup import Signup
 from resources.check_session import CheckSession
 from resources.logout import Logout
 from resources.events import Events
@@ -15,6 +16,7 @@ from resources.event_by_id import EventByID
 from resources.process_ticket import ProcessTicket
 from resources.ticket_by_id import TicketByID
 from resources.venue_by_id import VenueByID
+
 
 
 from flask import render_template
@@ -42,6 +44,7 @@ api.add_resource(Venues, '/api/venues')
 
 api.add_resource(Users, '/api/users')
 api.add_resource(Login, '/api/login')
+api.add_resource(Signup, '/api/signup')
 api.add_resource(CheckSession, '/api/check_session')
 api.add_resource(Logout, '/api/logout')
 api.add_resource(Events, '/api/events')
