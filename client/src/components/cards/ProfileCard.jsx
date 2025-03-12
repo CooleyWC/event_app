c151969d42c9f7a1dcb0ee35173af6612bd11e8e
Will Cooley <william.cooley@gmail.com>
2025-03-12 13:45:06 -0400

    added content and updated styling in profilecard

--- client/src/components/cards/ProfileCard.jsx
+++ client/src/components/cards/ProfileCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 function ProfileCard({user, firstName, lastName, location}) {
+
+    console.log('user', user)
+
+    const numOfTickets = user.tickets.length
+    const numOfEvents = user.created_events.length
     return (
-        <div className='bg-slate-200 flex justify-center flex-col'>
-            <h1 className='m-auto'>{firstName} {lastName}</h1>
-            <p className='m-auto'>{location}</p>
+        <div className='pt-10 flex justify-center flex-col text-white gap-4'>
+            <div>
+                <h1 className='text-4xl block text-center font-semibold'>{firstName} {lastName}</h1>
+                <p className='block text-center text-2xl'>{location}</p>
+            </div>
+            <div>
+                <p className='text-center'>Number of tickets purchased: {numOfTickets} </p>
+                <p className='text-center'>Number of events created: {numOfEvents} </p>
+            </div>
             
         </div>
     );
