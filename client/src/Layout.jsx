b3fb6d08b6b07d11135551a6b0fbe4983515fe75
Will Cooley <william.cooley@gmail.com>
2025-03-21 14:17:17 -0400

    added footer and review placeholder to layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { use } from 'react';
 import {useNavigate} from 'react-router-dom'
+import { FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa6";
 
 
 function Layout() {
@@ -15,6 +16,7 @@ function Layout() {
     return (
         <>
         <div className='mt-18  h-screen  bg-ivory dark:bg-gray-800 text-slate-800'>
+            {/* hero */}
             <div className='pt-32 md:pt-44 px-4 lg:pt-52 lg:px-10 w-full flex flex-col lg:flex-row gap-4 '>
                 <section className='w-full lg:w-[80%] xl:w-[70%] mx-auto flex flex-col gap-2 lg:gap-8'>
                     <h1 className='text-2xl md:text-4xl lg:text-5xl xl:text-8xl text-center font-mulish font-bold dark:text-ivory '>PARLOR STAGES</h1>
@@ -28,6 +30,7 @@ function Layout() {
                         />
                 </picture>
             </div>
+            {/* info cards */}
             <div className='bg-gray-300 mx-auto max-w-[1600px] w-full mt-14'>
                 <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center'>
                     <div className='py-6'>
@@ -51,6 +54,38 @@ function Layout() {
                     <button onClick={handleGetStartedClick} className='bg-gray-800 text-white font-bold py-2 px-4 mr-2 border-solid rounded border-ivory border hover:bg-gray-950 hover:text-white transition ease-in-out'>Get Started</button>
                 </div>
             </div>
+            {/* review carousel */}
+            <div className='h-[300px] bg-gray-200'>
+                <div>
+                    <h4 className='text-center'>Review Cards Will Go Here</h4>
+                </div>
+            </div>
+            {/* footer */}
+            <footer className='bg-gray-950 text-white flex flex-col sm:flex-row justify-between px-10'>
+                <div className='my-8'>
+                    <h5 className='font-bold text-lg mb-4'>How it works</h5>
+                    <a className='block text-sm'>FAQ for Artists and Presenters</a>
+                    <a className='block text-sm'>FAQ for Hosts and Venues</a>
+                </div>
+                <div className='my-8'>
+                    <h5 className='font-semibold mb-4'>About Us</h5>
+                    <a className='block text-sm'>Blog</a>
+                    <a className='block text-sm'>Jobs</a>
+                    <a className='block text-sm'>Press</a>
+                </div>
+                <div className='my-8'>
+                    <div className='flex flex-row gap-8 mb-4'>
+                        <FaInstagram className='text-2xl'/>
+                        <FaFacebook className='text-2xl'/>
+                        <FaYoutube className='text-2xl'/>
+                    </div>
+                    <div>
+                        <label>Signup for our newsletter</label>
+                        <input className='block'/>
+                    </div>
+                    
+                </div>
+            </footer>
         </div> 
         </>
     );
