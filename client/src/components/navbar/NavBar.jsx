35056e2c4493e1e9d1adbf9d02482af1017f1962
Will Cooley <william.cooley@gmail.com>
2025-03-20 21:34:50 -0400

    updated navbar styling in order to conditionally render the hamburger menu and logo based on screen size and url path

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {useState} from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import {IoMenu, IoSunnyOutline, IoMoonOutline, IoPersonOutline } from "react-icons/io5"
 import { useAuth } from '../context/AuthProvider';
 import ProfileDrawer from '../pages/ProfileDrawer';
@@ -9,18 +9,36 @@ function NavBar({toggleSideDrawer, toggleDarkMode, darkMode, toggleProfileDrawer
     const {user} = useAuth();
     const nameDisplay = user?.first_name + ' ' + user?.last_name
 
+    const [toggleTopnav, setToggleTopnav] = useState(false)
+
+    const location = useLocation()
+
+    const handleTopnavToggle = ()=>{
+        console.log('toggle')
+    }
+
+    let homePage = ''
+    if(location.pathname === '/'){
+        homePage = true
+    } else {
+        homePage = false
+    }
+
+
     return (
         <>
         <nav className='fixed top-0 z-50 w-full bg-white dark:bg-gray-950 text-black dark:text-ivory'>
             <div className='px-3 py-3 sm:py-5'>
                 <div className='flex items-center justify-between'>
                     <div className='flex items-center justify-start rtl:justify-end'>
-                        <button
+                        {location.pathname !== '/' && (
+                            <button
                             className='inline-flex items-center p-2 text-sm sm:hidden'
                             onClick={toggleSideDrawer}
                         >
-                            <IoMenu className='text-2xl' />
+                            <IoMenu className='text-2xl' onClick={handleTopnavToggle}/>
                         </button>
+                        )}
                         <div className='flex ms-2 md:me-24'>
                             <NavLink to='/'>
                                 <img src='./src/assets/images/parlor_logo_2.png' className='w-20 h-full'/>
@@ -47,7 +65,7 @@ function NavBar({toggleSideDrawer, toggleDarkMode, darkMode, toggleProfileDrawer
                                 <button onClick={toggleProfileDrawer} aria-label='Open profile drawer'>
                                     <div className='flex gap-2 items-center rounded-sm  text-black hover:bg-slate-200 dark:text-ivory dark:hover:bg-slate-900 p-1 sm:p-4 transition ease-in-out duration-300'>
                                         <IoPersonOutline className='size-8'/>
-                                        <span className='display-block text-lg'>{nameDisplay}</span>
+                                        <span className='hidden sm:block text-lg'>{nameDisplay}</span>
                                     </div>
                                 </button>
                             </>
